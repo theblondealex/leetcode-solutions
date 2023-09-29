c5eae56fc4b605fedc026e4f0244a82a63ca2582
TheBlondeAlex <65453441+Stephenstevenage@users.noreply.github.com>
2023-09-29 16:45:44 +0100

    finished 35 js

---
+++ JS/Top 100 liked/35. Search Insert Position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  if (nums.indexOf(target) !== -1) return nums.indexOf(target);
+  else {
+    nums.push(target);
+    nums.sort((a, b) => a - b);
+    return nums.indexOf(target);
+  }
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // 4
